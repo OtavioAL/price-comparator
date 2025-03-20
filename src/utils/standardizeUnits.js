b1f5c64ffdd890ac5595f3eb9796f7be2a1bff8b
OtavioAL <otavioauglei@gmail.com>
2025-03-20 20:14:14 -0300

    fix: transforming kilos into kg

--- src/utils/standardizeUnits.js
+++ src/utils/standardizeUnits.js
@@ -1,10 +1,11 @@
 // Função para padronizar unidades de medida
 function standardizeUnits(title) {
   return title
-    .replace(/\b1000g\b/gi, "1kg") // Transforma "1000g" em "1kg"
-    .replace(/\b1 quilo\b/gi, "1kg") // Transforma "1 Quilo" em "1kg"
-    .replace(/\b1 litro\b/gi, "1l") // Transforma "1 Litro" em "1L"
-    .replace(/\b2 litros\b/gi, "2l"); // Transforma "2 Litros" em "2L"
+    .replace(/\b1000g\b/gi, "1kg")
+    .replace(/\b1 quilo\b/gi, "1kg")
+    .replace(/\b5 quilos\b/gi, "5kg")
+    .replace(/\b1 litro\b/gi, "1l")
+    .replace(/\b2 litros\b/gi, "2l");
 }
 
 module.exports = { standardizeUnits };
